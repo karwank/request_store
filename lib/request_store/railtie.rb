3d4962fdbd9f8ae8d364be8f3362fd860c3fe88d
Karol Karwacki <karwackikarol@gmail.com>
2022-11-25 11:49:32 +0100

    fix

--- lib/request_store/railtie.rb
+++ lib/request_store/railtie.rb
@@ -1,15 +1,6 @@
 module RequestStore
   class Railtie < ::Rails::Railtie
     initializer "request_store.insert_middleware" do |app|
-      binding.pry
-      unless app.config.middleware.map(&:name).include? 'RequestStore::Middleware'
-        if ActionDispatch.const_defined? :RequestId
-          app.config.middleware.insert_after ActionDispatch::RequestId, RequestStore::Middleware
-        else
-          app.config.middleware.insert_after Rack::MethodOverride, RequestStore::Middleware
-        end
-      end
-
       if ActiveSupport.const_defined?(:Reloader) && ActiveSupport::Reloader.respond_to?(:to_complete)
         ActiveSupport::Reloader.to_complete do
           RequestStore.clear!
